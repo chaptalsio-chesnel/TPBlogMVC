e5be88e96e5a0e5509630406627ed22f79380632
chaptalsio-chesnel <felix.chesnel@siochaptalqper.fr>
2017-03-22 14:47:04 +0100

    Fin II-A : Deplacement de l'acces aux données dans un nouveau fichier dans un fonction getBillets()

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
-// Accès aux données
-$bdd = new PDO('mysql:host=localhost;dbname=tpblogmvc;charset=utf8', 'ts2', 'ts2');
-$billets = $bdd->query('select BIL_ID as id, BIL_DATE as date,'
-  . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'
-  . ' order by BIL_ID desc');
+//Acces aux données
+require 'Modele.php';
+
+$billets = getBillets();
 
 // Affichage
 require 'vueAccueil.php';
