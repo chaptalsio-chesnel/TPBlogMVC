a0c2af3986667f6289d53cfea211c8530840af83
chaptalsio-chesnel <felix.chesnel@siochaptalqper.fr>
2017-03-29 14:12:01 +0200

    Ajout commentaire

--- Controleur/Routeur.php
+++ Controleur/Routeur.php
@@ -17,28 +17,39 @@ class Routeur {
 	public function routerRequete() {
 		try {
 			if (isset($_GET['action'])) {
-				if ($_GET['action'] == 'billet') {
-					if (isset($_GET['id'])) {
-						$idBillet = intval($_GET['id']);
-						if ($idBillet != 0) {
-							$this->ctrlBillet->billet($idBillet);
-						}
-						else
-							throw new Exception("Identifiant de billet non valide");
-					}
-					else
-						throw new Exception("Identifiant de billet non défini");
-				}
-				else
-					throw new Exception("Action non valide");
-			}
-			else {  // aucune action définie : affichage de l'accueil
-				$this->ctrlAccueil->accueil();
-			}
+			    if ($_GET['action'] == 'billet') {
+			      $idBillet = intval($this->getParametre($_GET, 'id'));
+			      if ($idBillet != 0) {
+			        $this->ctrlBillet->billet($idBillet);
+			      }
+			      else
+			        throw new Exception("Identifiant de billet non valide");
+			    }
+			    else if ($_GET['action'] == 'commenter') {
+			      $auteur = $this->getParametre($_POST, 'auteur');
+			      $contenu = $this->getParametre($_POST, 'contenu');
+			      $idBillet = $this->getParametre($_POST, 'id');
+			      $this->ctrlBillet->commenter($auteur, $contenu, $idBillet);
+			    }
+			    else
+			      throw new Exception("Action non valide");
+			  }
+			  else {  // aucune action définie : affichage de l'accueil
+			    $this->ctrlAccueil->accueil();
+			  }
 		}
 		catch (Exception $e) {
 			$this->erreur($e->getMessage());
 		}
+	}
+	
+	// Recherche un paramètre dans un tableau
+	private function getParametre($tableau, $nom) {
+		if (isset($tableau[$nom])) {
+			return $tableau[$nom];
+		}
+		else
+			throw new Exception("Paramètre '$nom' absent");
 	}
 
 	// Affiche une erreur
--- Vue/vueBillet.php
+++ Vue/vueBillet.php
@@ -16,3 +16,13 @@
   <p><?= $commentaire['auteur'] ?> dit :</p>
   <p><?= $commentaire['contenu'] ?></p>
 <?php endforeach; ?>
+
+<hr/>
+<form method="post" action="index.php?action=commenter">
+    <input id="auteur" name="auteur" type="text" placeholder="Votre pseudo" 
+           required /><br />
+    <textarea id="txtCommentaire" name="contenu" rows="4" 
+              placeholder="Votre commentaire" required></textarea><br />
+    <input type="hidden" name="id" value="<?= $billet['id'] ?>" />
+    <input type="submit" value="Commenter" />
+</form>
